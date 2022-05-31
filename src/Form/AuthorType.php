14afa82db118595cfe4c9ea31e2451797d4f14a7
Rita <ritaclameira@gmail.com>
2022-05-31 14:21:21 +0200

    authors form personalization

--- src/Form/AuthorType.php
+++ src/Form/AuthorType.php
@@ -3,26 +3,38 @@
 namespace App\Form;
 
 use App\Entity\Author;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AuthorType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    { // form creation/personalization
         $builder
-            ->add('lastName')
-            ->add('firstName')
-            ->add('birthDate')
-            // ->add('books')
-        ;
+            ->add('lastName', null, [
+                'label' => 'Author\'s first name',
+                'attr' => ['placeholder' => 'First name'],
+                'required' => true
+            ])
+            ->add('firstName', null, [
+                'label' => 'Author\'s last name',
+                'attr' => ['placeholder' => 'Last name'],
+                'required' => true
+            ])
+            ->add('birthDate', DateType::class, [
+                'label' => 'Author\'s birth date',
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+                'data' => new \DateTime(),
+                'attr' => ['class' => 'form-control']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Author::class,
-        ]);
+        $resolver->setDefaults(['data_class' => Author::class]);
     }
 }
