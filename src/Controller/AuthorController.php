ebb72a7e96e1c679234c888b8e40ab59a7f2efd8
Rita <ritaclameira@gmail.com>
2022-05-31 12:20:41 +0200

    created form AuthorType and author view templates to display all authors and form to add new authors

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
+use App\Form\AuthorType;
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorController extends AbstractController
 {
+    private $authorRepo;
+
+    public function __construct(AuthorRepository $authorRepository)
+    {
+        $this->authorRepo = $authorRepository;
+    }
+
     /**
      * @Route("/author", name="app_author")
      */
-    public function index(): Response
+    public function listAuthor(): Response
     {
-        return $this->render('author/index.html.twig', [
-            'controller_name' => 'AuthorController',
+        $authors = $this->authorRepo->findAll();
+
+        return $this->render('author/listAuthor.html.twig', [
+            'authors' => $authors,
         ]);
     }
+
+    /**
+     * @Route("/addAuthor", name="app_newAuthor", methods={"GET", "POST"})
+     * @return void
+     */
+    public function addAuthor() // method get recovers teh form click and the method post recovers the form submition 
+    {
+        $formulary = $this->createForm(AuthorType::class);
+
+        return $this->render('author/editAuthor.html.twig', [
+            'form' => $formulary->createView(),
+        ]);
+    }
+
 }
---
+++ src/Form/AuthorType.php
@@ -0,0 +1,28 @@
+<?php
+// equivelent to Model
+namespace App\Form;
+
+use App\Entity\Author;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AuthorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('birthDate')
+            // ->add('books')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Author::class,
+        ]);
+    }
+}
