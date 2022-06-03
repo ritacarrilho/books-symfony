c85e0c6e256142f5c39f55e3685a9f2393f8fe96
Rita <ritaclameira@gmail.com>
2022-06-03 11:16:48 +0200

    installed webpack, SASS, Bootstrap, Jquery

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,14 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/style.sass';
 
 // start the Stimulus application
 import './bootstrap';
+
+const $ = require('jquery');
+require('bootstrap');
+
+$(document).ready(function() { // when the page finishes to load
+    $('[data-bs-toggle = "tooltip"]').tooltip(); // activates tooltip to all elements where it was applied 
+});
